334b5e70fc8c3715b1efe01a95fc8c2425023628
Fernando Antunes <fernando.antunes1@gmail.com>
2020-08-27 17:59:59 -0300

    exercicio2

---
+++ desenvolvimento-front-end/bloco4/dia_1/exercicio2.js
@@ -0,0 +1,11 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let valor1 = 51;
+let valor2 = 63;
+
+if (valor1 > valor2) {
+    console.log (valor1, "é maior que", valor2)
+}
+else {
+    console.log (valor2, "é maior que", valor1)
+}

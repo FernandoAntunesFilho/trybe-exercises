5e74bcd3214e0e2eada5a09e75acd6beb9f01e8b
Fernando Antunes <fernando.antunes1@gmail.com>
2020-08-31 19:32:20 -0300

    subindo novamente os exercicios 1 e 2. e como novo o exercicio 3

--- desenvolvimento-front-end/bloco4/dia_3/exercicio1.js
+++ desenvolvimento-front-end/bloco4/dia_3/exercicio1.js
@@ -1,13 +1,10 @@
 let n = 5;
 let linha = "";
 
-for (let i = 1; i <= n; i += 1) {
-    linha = linha + "*"
-    console.log(linha);
+for (let i=1; i<=n; i+=1) {
+    linha = linha + "*";
 }
 
-// for (let i = 1; i<= n; i += 1) {
-//     console.log(linha);
-// }
-
-//console.log(n, linha);
+for (let i=1; i<=n; i+=1) {
+    console.log(linha);
+}
---
+++ desenvolvimento-front-end/bloco4/dia_3/exercicio2.js
@@ -0,0 +1,11 @@
+let n = 5;
+let linha = "";
+
+for (let i=1; i<=n; i+=1) {
+    linha = linha + "*";
+    console.log(linha);
+}
+
+// for (let i=1; i<=n; i+=1) {
+//     console.log(linha);
+// }

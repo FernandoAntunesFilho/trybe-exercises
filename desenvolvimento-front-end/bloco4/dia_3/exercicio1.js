b49e550562337e225ca80b8562880f2079a331e6
Fernando Antunes <fernando.antunes1@gmail.com>
2020-08-31 18:19:36 -0300

    primeiro commit-exercicio1

---
+++ desenvolvimento-front-end/bloco4/dia_3/exercicio1.js
@@ -0,0 +1,12 @@
+let n = 5;
+let linha = "";
+
+for (let i = 1; i <= n; i += 1) {
+    linha = linha + "*"
+}
+
+for (let i = 1; i<= n; i += 1) {
+    console.log(linha);
+}
+
+//console.log(n, linha);
